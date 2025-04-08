f9ea36ac3da2419865910e7d16ccc605e312107a
shubhashishneutroline <shubhashish.neutroline@gmail.com>
2025-04-08 10:47:30 +0545

    email setup

--- src/tasks/inngest/function.ts
+++ src/tasks/inngest/function.ts
@@ -1,5 +1,6 @@
 import { inngestClient } from "@/tasks/inngest/client";
 import { prisma } from "@/lib/prisma";
+import { sendReminderEmail } from "../email/email";
 
 export const cronReminder = inngestClient.createFunction(
   {
@@ -7,7 +8,8 @@ export const cronReminder = inngestClient.createFunction(
     name: "Send Reminder (CRON)",
   },
   {
-    cron: "0 * * * *", // every 1 hr
+   /*  cron: "0 * * * *", */ // every 1 hr
+    cron: "* * * * *"
   },
   async ({ step }) => {
     // Step 1: Fetch all reminders with relations
@@ -21,20 +23,26 @@ export const cronReminder = inngestClient.createFunction(
       });
     });
  */
+
+    //perform db relations with user to fetch the email and name
     const reminders: any[] = [
       {
         send48hr: "scheduled",
         send24Hr: "scheduled",
         send1Hr: "scheduled",
         sendOffset: "2880",
-        scheduledAt: "2025-04-08T13:34:56.789Z",
+        scheduledAt: "2025-04-10T13:34:56.789Z",
+        email: "jack.2smith45@gmail.com",
+        name: "Jack Smith"
       },
       {
-        send48hr: "sent",
+        send48hr: "scheduled",
         send24Hr: "scheduled",
         send1Hr: "scheduled",
-        sendOffset: "2880",
-        scheduledAt: "2025-04-06T12:00:56.789Z",
+        sendOffset: "1440",
+        scheduledAt: "2025-04-09T09:34:56.789Z",
+        email: "sarahdoe982@gmail.com",
+         name: "Sarah Doe"
       },
     ];
 
@@ -56,28 +64,37 @@ export const cronReminder = inngestClient.createFunction(
         // Compare with 48 hours (2880 minutes), 24 hours (1440 minutes), and 1 hour (60 minutes)
         if (diffInMinutes <= 105 && diffInMinutes > 1 && reminder.send1Hr === "scheduled") {
           console.log("Around 1-hour left for appointment ...");
+          
+          //send email
+          await sendReminderEmail(reminder.email, reminder.name, reminder.sendOffset)
+
           // Send reminder for 48 hours
-          reminder.send1Hr = "sent"; // Update the status to sent
+          reminder.send1Hr = "sent"; // Update the status to sent in db for send1hr
         } else if (
           diffInMinutes <= 36 * 60 &&
           diffInMinutes > 12 * 60 &&
           reminder.send24Hr === "scheduled"
         ) {
           console.log("Around 24-hour left for appointment ...");
+
+          await sendReminderEmail(reminder.email, reminder.name, reminder.sendOffset)
           // Send reminder for 24 hours
-          reminder.send24Hr = "sent"; // Update the status to sent
+          reminder.send24Hr = "sent"; // Update the status to sent in db for send24hr
         } else if (
           diffInMinutes <= 56 * 60 &&
           diffInMinutes > 36 * 60 &&
           reminder.send48hr === "scheduled"
         ) {
           console.log("Around 48-hour left for appointment ...");
+           //send email
+           await sendReminderEmail(reminder.email, reminder.name, reminder.sendOffset)
+
           // Send reminder for 1 hour
-          reminder.send48hr = "sent"; // Update the status to sent
+          reminder.send48hr = "sent"; // Update the status to sent in db for send48hr
         }
       }
       console.log('reminder',reminders);
-      return reminders;
+     return reminders;
     });
   }
 );
